2524614af67ff5b3d489e5104ba6f8ec47e32add
Davide Tavella <tavella01@gmail.com>
2021-12-09 21:14:30 +0100

    es 4

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -27,42 +27,53 @@ checkAge();
 // no
 // si, non si può usare una const se poi in base ad una variabile deve cambiare il valore di questa const. Serve una variabile let
 
-// // ESERCIZIO 2
-// function printColorsNumber() {
-//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-// }
-// printColorsNumber();
+// ESERCIZIO 2
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
 
+// è una funzione che analizza il numero di colori presenti nell'array "colors" per scrivere il numero all'interno della frase in console
+// si, length e non lenght :)
+// no
 
-// // ESERCIZIO 3
-// function addNumbers() {
-//     const userNumber = prompt('Inserisci un numero');
-//     const total = userNumber + 12;
+// ESERCIZIO 3
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
 
-//     console.log(`Il risultato finale è ${total}`);
-// }
-// addNumbers();
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
 
+// è una funzione che somma il numero che inserisci l'utente e ti stampa il risultato
+// no
+// si, non si può usare solo prompt, perchè se no js prende ciò che scriviamo come stringa e lo concatena a 12, serve il parseInt per dire a js che è un intero
 
-// // ESERCIZIO 4
-// function checkAccess() {
-//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+// ESERCIZIO 4
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-//     let grantAccess = 'false';
+    let grantAccess = false;
 
-//     if (addresses.includes(userEmail)) {
-//         grantAccess = 'true';
-//     }
+    if (addresses.includes(userEmail)) {
+        grantAccess = true;
+    }
 
-//     if (grantAccess === true) {
-//         console.log('Accesso consentito!');
-//     } else {
-//         console.log('Accesso negato!');
-//     }
-// }
-// checkAccess();
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
+
+// è una funzione che somma il numero che inserisci l'utente e ti stampa il risultato
+// si, false e true (valori booleani) non vanno messi tra virgolette come se fossero stringhe
+// no
+
 
 
 // // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -92,22 +103,6 @@ checkAge();
 //     }
 // }
 // checkAccessImproved();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
