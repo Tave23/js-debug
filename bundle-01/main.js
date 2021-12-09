ede5ebf38a07d1bec31d091924b16c13db3eff1e
Davide Tavella <tavella01@gmail.com>
2021-12-09 20:35:36 +0100

    es 3-4

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -29,27 +29,40 @@ function addIfEven(num) {
 }
 
 // è una funzione che analizza "num" e qualora fosse pari, aggiunge 5 a "num"
-// si, non bisogna aggiugnere = 0 perché il simbolo % verifica se è divisibile per X con resto 0, non il risultato = 0
+// si, non bisogna aggiugnere = 0, ma === 0, perché il simbolo % verifica se è divisibile per X con resto 0, non il risultato = 0
 // più o meno, era errato il significato di %. Avrebbe dovuto usare "/" per effettuare una divisione
 
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
+
+// è una funzione con un ciclo for al suo interno, che cicla i per 4 volte.
+// si, all'interno delle parentesi del ciclo for, non vanno usate le virgole ma il punto e virgola.
+// nessun errore logico
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
         return evenNumbers;
     }
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+// è una funzione che deve restituire solo i numeri pari presenti nell'array "numbers"
+// si:
+// c'è un ; di troppo nel ciclo for.
+// lo stesso errore dell'esercizio 2 (x % 2 === 0)
+// il punto e virgola dopo la parentesi dove è presente il modulo
+// numbers.length - 1 è sbagliato, perché prenderebbe solo 7 elementi
+// non si può verificare se un array è divisibile per 2, si può verificare se il suo indice lo è (numbers[i])
+// no, non mi sembra ci sia un errore logico importante, forse solo quello dell'indice dell'array
